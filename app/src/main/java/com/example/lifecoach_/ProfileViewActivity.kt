54e3ca28f0cdb43933ae66b7eb33dd8dcd631669
juanfra312003 <80067016+juanfra312003@users.noreply.github.com>
2023-08-19 14:19:39 -0500

    feat-correction-id-items-bottom-navegation-bar

--- app/src/main/java/com/example/lifecoach_/ProfileViewActivity.kt
+++ app/src/main/java/com/example/lifecoach_/ProfileViewActivity.kt
@@ -1,29 +1,33 @@
-package com.example.lifecoach_
-
-import android.graphics.Color
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
-import android.view.Window
-import android.view.WindowId
+import androidx.appcompat.app.AppCompatActivity
+import com.example.lifecoach_.R
 import com.example.lifecoach_.databinding.ActivityProfileViewBinding
 
 class ProfileViewActivity : AppCompatActivity() {
 
-    private lateinit var binding : ActivityProfileViewBinding
-
+    private lateinit var binding: ActivityProfileViewBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
-        binding = ActivityProfileViewBinding.inflate(layoutInflater)
         super.onCreate(savedInstanceState)
+        binding = ActivityProfileViewBinding.inflate(layoutInflater)
         setContentView(binding.root)
-    }
 
-    fun manageBottomNavBar(){
-        binding.bottomNavigationView.setOnClickListener{
-            when (it.id){
-
+        binding.bottomNavigationView.setOnNavigationItemSelectedListener { menuItem ->
+            when (menuItem.itemId) {
+                R.id.profileMenu-> {
+                    //Use the activity of
+                    val intent = Intent(this, HomeActivity::class.java)
+                    startActivity(intent)
+                    true
+                }
+                R.id.menu -> {
+                    // Ya estás en la actividad de perfil, no es necesario hacer nada
+                    true
+                }
+                // Agrega más casos para otros items si es necesario
+                else -> false
             }
         }
     }
-
 }
