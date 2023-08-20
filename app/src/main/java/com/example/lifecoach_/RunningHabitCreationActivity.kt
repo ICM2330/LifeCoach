0dcfd67948465d0f37693040ede30042ddc337f3
Juan Camilo Sánchez Urrego <jcsu2019369@gmail.com>
2023-08-19 21:18:02 -0500

    Feat: Creates Running Habit Activity

---
+++ app/src/main/java/com/example/lifecoach_/RunningHabitCreationActivity.kt
@@ -0,0 +1,11 @@
+package com.example.lifecoach_
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class RunningHabitCreationActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_running_habit_creation)
+    }
+}
