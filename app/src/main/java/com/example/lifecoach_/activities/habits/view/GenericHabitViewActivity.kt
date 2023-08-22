5e2ef30a315a34f4d97900d34a3099292d154c87
Sofía Céspedes Vargas <sofivampaireknight@gmail.com>
2023-08-22 18:55:02 -0500

    Pantalla habito generico

---
+++ app/src/main/java/com/example/lifecoach_/activities/habits/view/GenericHabitViewActivity.kt
@@ -0,0 +1,12 @@
+package com.example.lifecoach_.activities.habits.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.lifecoach_.R
+
+class GenericHabitViewActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_generic_habit_view)
+    }
+}
