1392d494d628d0acef741555b69ab41f3c8ff439
Juan Camilo Sánchez Urrego <jcsu2019369@gmail.com>
2023-08-22 18:24:50 -0500

    Feat: Vista Habito Pasos

---
+++ app/src/main/java/com/example/lifecoach_/activities/habits/view/StepHabitViewActivity.kt
@@ -0,0 +1,12 @@
+package com.example.lifecoach_.activities.habits.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.lifecoach_.R
+
+class StepHabitViewActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_step_habit_view)
+    }
+}
