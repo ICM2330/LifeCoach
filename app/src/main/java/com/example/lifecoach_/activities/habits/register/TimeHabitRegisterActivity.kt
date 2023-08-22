e64a7ac4aaccd61b19bebd901906a9b5cac53978
Juan Camilo Sánchez Urrego <jcsu2019369@gmail.com>
2023-08-22 18:34:12 -0500

    Feat: Vista Registro del Hábito Tiempo

---
+++ app/src/main/java/com/example/lifecoach_/activities/habits/register/TimeHabitRegisterActivity.kt
@@ -0,0 +1,12 @@
+package com.example.lifecoach_.activities.habits.register
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.lifecoach_.R
+
+class TimeHabitRegisterActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_time_habit_register)
+    }
+}
