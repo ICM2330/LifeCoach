4c425203f1405bb80786ffe84b0ce748f2dc3404
Juan Camilo Sánchez Urrego <jcsu2019369@gmail.com>
2023-08-22 17:40:03 -0500

    Feat: Agrega Botón de Creación del Hábito

---
+++ app/src/main/java/com/example/lifecoach_/activities/habits/view/RunningHabitViewActivity.kt
@@ -0,0 +1,12 @@
+package com.example.lifecoach_.activities.habits.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.lifecoach_.R
+
+class RunningHabitViewActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_running_habit_view)
+    }
+}
