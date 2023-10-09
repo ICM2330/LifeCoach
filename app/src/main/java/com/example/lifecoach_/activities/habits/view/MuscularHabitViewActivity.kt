f3655cc9968990abb2bbc55423e138c8568f39c6
juanfra312003 <juanframireze@javeriana.edu.co>
2023-10-09 14:17:28 -0500

    feat-fix-layout-view-strenght-habit-with-interactivty

--- app/src/main/java/com/example/lifecoach_/activities/habits/view/MuscularHabitViewActivity.kt
+++ app/src/main/java/com/example/lifecoach_/activities/habits/view/MuscularHabitViewActivity.kt
@@ -1,12 +1,29 @@
 package com.example.lifecoach_.activities.habits.view
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.example.lifecoach_.R
+import com.example.lifecoach_.activities.habits.auxiliar.SearchGymForHabitActivity
+import com.example.lifecoach_.databinding.ActivityMuscularHabitViewBinding
 
 class MuscularHabitViewActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMuscularHabitViewBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_muscular_habit_view)
+        binding = ActivityMuscularHabitViewBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        manageButtons()
+    }
+
+    private fun manageButtons(){
+        manageButtonGoToSearchGymForHabitActivity()
+    }
+
+    private fun manageButtonGoToSearchGymForHabitActivity(){
+        binding.buttonGimnasiosSearch.setOnClickListener {
+            startActivity(Intent(baseContext, SearchGymForHabitActivity::class.java))
+        }
     }
 }
