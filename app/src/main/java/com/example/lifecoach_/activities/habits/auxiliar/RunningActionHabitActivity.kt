f1682117a56c7722f8117afbe998d0b50f992a00
juanfra312003 <juanframireze@javeriana.edu.co>
2023-10-15 11:10:02 -0500

    feat-inti-chronometer

--- app/src/main/java/com/example/lifecoach_/activities/habits/auxiliar/RunningActionHabitActivity.kt
+++ app/src/main/java/com/example/lifecoach_/activities/habits/auxiliar/RunningActionHabitActivity.kt
@@ -2,17 +2,40 @@ package com.example.lifecoach_.activities.habits.auxiliar
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.SystemClock
 import com.example.lifecoach_.R
 import com.example.lifecoach_.databinding.ActivityRunningActionHabitBinding
 
 class RunningActionHabitActivity : AppCompatActivity() {
     private lateinit var binding : ActivityRunningActionHabitBinding
+    private var running = false
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityRunningActionHabitBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        // Do a chronometer since the activity is loaded
+        manageButtons()
+    }
 
+    private fun manageButtons() {
+        binding.actionsRunningButton.setOnClickListener {
+            if (!running){
+                // Begin running
+                running = true
+                binding.chronometerRunning.base = SystemClock.elapsedRealtime()
+                binding.chronometerRunning.start()
+
+                // Change button text
+                binding.actionsRunningButton.text = "Detener"
+            }
+            else{
+                // Stop running
+                running = false
+                binding.chronometerRunning.stop()
+
+                // Change button text
+                binding.actionsRunningButton.text = "Iniciar"
+            }
+        }
     }
 }
