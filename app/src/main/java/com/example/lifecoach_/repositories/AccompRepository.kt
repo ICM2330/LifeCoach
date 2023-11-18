9b7eb3cfa64dcd7f5ca5f9b3d0649b5d0d21f819
Juan Camilo Sánchez Urrego <jcsu2019369@gmail.com>
2023-11-18 13:16:54 -0500

    Feat: Esttructura Guardado de Accomps

---
+++ app/src/main/java/com/example/lifecoach_/repositories/AccompRepository.kt
@@ -0,0 +1,9 @@
+package com.example.lifecoach_.repositories
+
+import com.example.lifecoach_.model.habits.Accomplishment
+
+class AccompRepository {
+    fun addAccomp(accomplishment: Accomplishment, callback: (accomplishment: Accomplishment) -> Unit) {
+
+    }
+}
--- app/src/main/java/com/example/lifecoach_/services/firebase/HabitsService.kt
+++ app/src/main/java/com/example/lifecoach_/services/firebase/HabitsService.kt
@@ -1,17 +1,25 @@
 package com.example.lifecoach_.services.firebase
 
+import com.example.lifecoach_.model.habits.Accomplishment
 import com.example.lifecoach_.model.habits.Habit
+import com.example.lifecoach_.repositories.AccompRepository
 import com.example.lifecoach_.repositories.HabitRepository
 
 class HabitsService {
     private val habitRepository: HabitRepository = HabitRepository()
+    private val accompRepository: AccompRepository = AccompRepository()
 
     fun addHabit(habit: Habit, callback: (Habit) -> Unit) {
         // Agrega el Hábito
-        habitRepository.addHabit(habit) {
-            // TODO: Agrega todos los cumplimientos (Accomplishment)s relacionados
+        habitRepository.addHabit(habit) {habitWithID: Habit ->
+            // Agrega todos los cumplimientos (Accomplishment)s relacionados
+            habitWithID.accomplishment.forEach { accomplishment: Accomplishment ->
+                accompRepository.addAccomp(accomplishment) {
+
+                }
+            }
             // Retorna el hábito con el ID obtenido de Firebase
-            callback(habit)
+            callback(habitWithID)
         }
     }
 }
