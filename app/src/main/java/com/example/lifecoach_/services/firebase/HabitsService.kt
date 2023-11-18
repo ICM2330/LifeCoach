30efdc74dbe1257d7207f50ee7d4d562f367f483
Juan Camilo Sánchez Urrego <jcsu2019369@gmail.com>
2023-11-18 17:10:28 -0500

    Feat: Implementa Guardado Completo del Habito

--- app/src/main/java/com/example/lifecoach_/services/firebase/HabitsService.kt
+++ app/src/main/java/com/example/lifecoach_/services/firebase/HabitsService.kt
@@ -13,15 +13,33 @@ class HabitsService {
         // Agrega el Hábito
         habitRepository.addHabit(habit) {habitWithID: Habit ->
             // Agrega todos los cumplimientos (Accomplishment)s relacionados
-            habitWithID.accomplishment.forEach { accomplishment: Accomplishment ->
-                habitWithID.id?.let { hid: String ->
-                    accompRepository.addAccomp(accomplishment, hid) {
+            habitWithID.id?.let {hid: String ->
+                addsAccomps(habitWithID.accomplishment, hid)
+                {
+                    // Anexa los Accomps con IDs
+                    habitWithID.accomplishment = it
 
-                    }
+                    // Retorna el Hábito con IDs
+                    callback(habitWithID)
                 }
             }
-            // Retorna el hábito con el ID obtenido de Firebase
-            callback(habitWithID)
+        }
+    }
+
+    private fun addsAccomps(
+        accomps: MutableList<Accomplishment>,
+        habitId: String,
+        callback: (MutableList<Accomplishment>) -> Unit) {
+        val accompsWithId = mutableListOf<Accomplishment>()
+        accomps.forEachIndexed { index: Int, accomplishment: Accomplishment ->
+            accompRepository.addAccomp(accomplishment, habitId)
+            { accomplishmentWithID: Accomplishment ->
+                accompsWithId.add(accomplishmentWithID)
+                if (accompsWithId.size == accomps.size) {
+                    // Retorna los Accomplishments con los IDS
+                    callback(accompsWithId)
+                }
+            }
         }
     }
 }
