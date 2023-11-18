df03191f72895eff82ceddfc42a54f14e2615218
Juan Camilo Sánchez Urrego <jcsu2019369@gmail.com>
2023-11-18 12:45:05 -0500

    Feat: Estrcutura de HabitRepository

---
+++ app/src/main/java/com/example/lifecoach_/repositories/HabitRepository.kt
@@ -0,0 +1,11 @@
+package com.example.lifecoach_.repositories
+
+import com.example.lifecoach_.model.habits.Habit
+
+class HabitRepository {
+    fun addHabit(habit: Habit, callback: (Habit) -> Unit) {
+        // TODO: Mapea el Habito a un HashMap
+        // TODO: Carga el HashMap en Firebase
+        // TODO: Retorna el Habito con el ID
+    }
+}
--- app/src/main/java/com/example/lifecoach_/services/firebase/HabitsService.kt
+++ app/src/main/java/com/example/lifecoach_/services/firebase/HabitsService.kt
@@ -1,12 +1,19 @@
 package com.example.lifecoach_.services.firebase
 
 import com.example.lifecoach_.model.habits.Habit
+import com.example.lifecoach_.repositories.HabitRepository
 
 class HabitsService {
+    private val habitRepository: HabitRepository = HabitRepository()
+
     fun addHabit(habit: Habit, callback: (Habit) -> Unit) {
         // Agrega el Hábito
-        // Agrega frecuencia
-        // Agrega días de la semana a las frecuencias
-        // Agrega todos los cumplimientos (Accomplishment)s relacionados
+        habitRepository.addHabit(habit) {
+            // TODO: Agrega frecuencia
+            // TODO: Agrega días de la semana a las frecuencias
+            // TODO: Agrega todos los cumplimientos (Accomplishment)s relacionados
+            // Retorna el hábito con el ID obtenido de Firebase
+            callback(habit)
+        }
     }
 }
