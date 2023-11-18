7a13429ad9324d4e4036169b472f898917d9e818
Juan Camilo Sánchez Urrego <jcsu2019369@gmail.com>
2023-11-18 12:57:20 -0500

    Feat: Implementa el HabitMapper.kt

---
+++ app/src/main/java/com/example/lifecoach_/mappers/HabitMapper.kt
@@ -0,0 +1,17 @@
+package com.example.lifecoach_.mappers
+
+import com.example.lifecoach_.model.habits.Habit
+import com.google.gson.Gson
+
+class HabitMapper {
+    private val gson = Gson()
+    fun habitToMap(habit: Habit): HashMap<String, Any?> {
+        return hashMapOf<String, Any?>(
+            "id" to habit.id,
+            "name" to habit.name,
+            "freq_hour" to habit.frequency.notiHour,
+            "freq_min" to habit.frequency.notiMinute,
+            "freq_days" to gson.toJson(habit.frequency.days)
+        )
+    }
+}
--- app/src/main/java/com/example/lifecoach_/repositories/HabitRepository.kt
+++ app/src/main/java/com/example/lifecoach_/repositories/HabitRepository.kt
@@ -1,10 +1,15 @@
 package com.example.lifecoach_.repositories
 
+import com.example.lifecoach_.mappers.HabitMapper
 import com.example.lifecoach_.model.habits.Habit
 
 class HabitRepository {
+    private val habitMapper: HabitMapper = HabitMapper()
+
     fun addHabit(habit: Habit, callback: (Habit) -> Unit) {
-        // TODO: Mapea el Habito a un HashMap
+        // Mapea el Habito a un HashMap
+        val habitHashMap = habitMapper.habitToMap(habit)
+
         // TODO: Carga el HashMap en Firebase
         // TODO: Retorna el Habito con el ID
     }
