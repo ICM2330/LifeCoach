69d8999e12f8d57678b9021519e2027ffb75d744
Juan Camilo Sánchez Urrego <jcsu2019369@gmail.com>
2023-11-18 12:41:38 -0500

    Feat: Estrcutura del Servicio de Hábitos

--- app/src/main/java/com/example/lifecoach_/controllers/activities_controllers/activity_dashboard/DashBoardHabitsDataController.kt
+++ app/src/main/java/com/example/lifecoach_/controllers/activities_controllers/activity_dashboard/DashBoardHabitsDataController.kt
@@ -1,10 +1,13 @@
 package com.example.lifecoach_.controllers.activities_controllers.activity_dashboard
 
 import com.example.lifecoach_.model.habits.Habit
+import com.example.lifecoach_.services.firebase.HabitsService
 
 class DashBoardHabitsDataController {
-    fun addHabit(habit: Habit, callback: (Habit) -> Unit) {
+    private val habitsService: HabitsService = HabitsService()
 
+    fun addHabit(habit: Habit, callback: (Habit) -> Unit) {
+        habitsService.addHabit(habit, callback)
     }
 
     fun updateHabit(habit: Habit, callback: () -> Unit) {
---
+++ app/src/main/java/com/example/lifecoach_/services/firebase/HabitsService.kt
@@ -0,0 +1,12 @@
+package com.example.lifecoach_.services.firebase
+
+import com.example.lifecoach_.model.habits.Habit
+
+class HabitsService {
+    fun addHabit(habit: Habit, callback: (Habit) -> Unit) {
+        // Agrega el Hábito
+        // Agrega frecuencia
+        // Agrega días de la semana a las frecuencias
+        // Agrega todos los cumplimientos (Accomplishment)s relacionados
+    }
+}
