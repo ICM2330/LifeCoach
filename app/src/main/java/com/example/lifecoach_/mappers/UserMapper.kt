5d377b8f23782dc4e6d7f96fb40608f4a5ac0854
Juan Camilo Sánchez Urrego <jcsu2019369@gmail.com>
2023-11-18 22:18:30 -0500

    Fix: UserMapper eliminando la foto del usuario

--- app/src/main/java/com/example/lifecoach_/mappers/UserMapper.kt
+++ app/src/main/java/com/example/lifecoach_/mappers/UserMapper.kt
@@ -4,13 +4,23 @@ import com.example.lifecoach_.model.User
 
 class UserMapper {
     fun userToMap(user: User, picRef: String?): HashMap<String, Any?> {
-        return hashMapOf<String, Any?>(
-            "uid" to user.uid,
-            "name" to user.name,
-            "username" to user.username,
-            "email" to user.email,
-            "phone" to user.phone,
-            "picture" to (picRef ?: "")
-        )
+        if (picRef != null) {
+            return hashMapOf<String, Any?>(
+                "uid" to user.uid,
+                "name" to user.name,
+                "username" to user.username,
+                "email" to user.email,
+                "phone" to user.phone,
+                "picture" to picRef
+            )
+        } else {
+            return hashMapOf<String, Any?>(
+                "uid" to user.uid,
+                "name" to user.name,
+                "username" to user.username,
+                "email" to user.email,
+                "phone" to user.phone
+            )
+        }
     }
 }
