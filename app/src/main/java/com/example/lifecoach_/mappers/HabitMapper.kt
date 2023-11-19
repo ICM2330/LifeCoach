e31371450a902adefe55072b79af7343845c5f86
Juan Camilo Sánchez Urrego <jcsu2019369@gmail.com>
2023-11-18 20:27:16 -0500

    Feat: Agrega el mapeo de hashmap a habito

--- app/src/main/java/com/example/lifecoach_/mappers/HabitMapper.kt
+++ app/src/main/java/com/example/lifecoach_/mappers/HabitMapper.kt
@@ -1,7 +1,9 @@
 package com.example.lifecoach_.mappers
 
+import com.example.lifecoach_.model.habits.Frequency
 import com.example.lifecoach_.model.habits.Habit
 import com.google.gson.Gson
+import com.google.gson.reflect.TypeToken
 
 class HabitMapper {
     private val gson = Gson()
@@ -11,6 +13,18 @@ class HabitMapper {
             "freq_hour" to habit.frequency.notiHour,
             "freq_min" to habit.frequency.notiMinute,
             "freq_days" to gson.toJson(habit.frequency.days)
+        )
+    }
+
+    fun mapToHabit(docId: String, habitHashMap: HashMap<String, Any?>): Habit {
+        return Habit(
+            docId,
+            habitHashMap["name"] as String,
+            Frequency(
+                habitHashMap["freq_hour"] as Int,
+                habitHashMap["freq_min"] as Int,
+                gson.fromJson(habitHashMap["freq_days"] as String, object: TypeToken<MutableList<Int>>(){}.type)
+            )
         )
     }
 }
