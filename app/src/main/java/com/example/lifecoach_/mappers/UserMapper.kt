f7b6b98e5db18ae94c2f22302b2574e9cab965f6
Juan Camilo Sánchez Urrego <jcsu2019369@gmail.com>
2023-11-18 22:20:37 -0500

    Revert: UserMapper eliminando la foto del usuario

--- app/src/main/java/com/example/lifecoach_/mappers/UserMapper.kt
+++ app/src/main/java/com/example/lifecoach_/mappers/UserMapper.kt
@@ -4,23 +4,13 @@ import com.example.lifecoach_.model.User
 
 class UserMapper {
     fun userToMap(user: User, picRef: String?): HashMap<String, Any?> {
-        if (picRef != null) {
-            return hashMapOf<String, Any?>(
-                "uid" to user.uid,
-                "name" to user.name,
-                "username" to user.username,
-                "email" to user.email,
-                "phone" to user.phone,
-                "picture" to picRef
-            )
-        } else {
-            return hashMapOf<String, Any?>(
-                "uid" to user.uid,
-                "name" to user.name,
-                "username" to user.username,
-                "email" to user.email,
-                "phone" to user.phone
-            )
-        }
+        return hashMapOf<String, Any?>(
+            "uid" to user.uid,
+            "name" to user.name,
+            "username" to user.username,
+            "email" to user.email,
+            "phone" to user.phone,
+            "picture" to (picRef ?: "")
+        )
     }
 }
