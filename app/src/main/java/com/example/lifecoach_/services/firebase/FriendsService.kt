afa0298f213ad5b97226d471cf641e2a170a26e1
Juan Camilo Sánchez Urrego <jcsu2019369@gmail.com>
2023-11-18 23:37:22 -0500

    Feat: Implementacion Parcial de FriendsService

--- app/src/main/java/com/example/lifecoach_/services/firebase/FriendsService.kt
+++ app/src/main/java/com/example/lifecoach_/services/firebase/FriendsService.kt
@@ -1,18 +1,25 @@
 package com.example.lifecoach_.services.firebase
 
+import com.example.lifecoach_.mappers.converters.UserFriendMapper
 import com.example.lifecoach_.model.Friend
 import com.example.lifecoach_.repositories.UserRepository
 
 class FriendsService {
     private val userRepository = UserRepository()
+    private val userFriendMapper = UserFriendMapper()
 
     fun getAllFriends(callback: (MutableList<Friend>) -> Unit) {
-        userRepository.getAllUsers {
-            // TODO: Mapear todos los usuarios a amigos
+        userRepository.getAllUsers {users ->
+            // Mapear todos los usuarios a amigos
+            val friends = mutableListOf<Friend>()
+            users.forEach {user ->
+                friends.add(userFriendMapper.userToFriend(user))
+            }
 
             // TODO: Registrar Listener y Petición para los Mensajes
 
-            // TODO: Retornar Lista de Amigos
+            // Retornar Lista de Amigos
+            callback(friends)
         }
     }
 }
