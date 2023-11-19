8956abf2ab1467f6ee7583f698236f26f10c08b3
Juan Camilo Sánchez Urrego <jcsu2019369@gmail.com>
2023-11-18 22:36:44 -0500

    Feat: Estructura de Actualización de Amigos

---
+++ app/src/main/java/com/example/lifecoach_/controllers/activities_controllers/activity_chat_menu/ChatMenuDataController.kt
@@ -0,0 +1,9 @@
+package com.example.lifecoach_.controllers.activities_controllers.activity_chat_menu
+
+import com.example.lifecoach_.model.Friend
+
+class ChatMenuDataController {
+    fun registerFriendsListener(callback: (MutableList<Friend>) -> Unit) {
+
+    }
+}
