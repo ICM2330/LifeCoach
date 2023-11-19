5424ab61222480001ed0dcb2dae4bec59e9b8e3b
Juan Camilo Sánchez Urrego <jcsu2019369@gmail.com>
2023-11-18 23:39:01 -0500

    Feat: Conecta el FriendsService con el Controlador de la Pantalla de Chats

--- app/src/main/java/com/example/lifecoach_/controllers/activities_controllers/activity_chat_menu/ChatMenuDataController.kt
+++ app/src/main/java/com/example/lifecoach_/controllers/activities_controllers/activity_chat_menu/ChatMenuDataController.kt
@@ -1,9 +1,12 @@
 package com.example.lifecoach_.controllers.activities_controllers.activity_chat_menu
 
 import com.example.lifecoach_.model.Friend
+import com.example.lifecoach_.services.firebase.FriendsService
 
 class ChatMenuDataController {
-    fun registerFriendsListener(callback: (MutableList<Friend>) -> Unit) {
+    private val friendsService = FriendsService()
 
+    fun registerFriendsListener(callback: (MutableList<Friend>) -> Unit) {
+        friendsService.getAllFriends(callback)
     }
 }
