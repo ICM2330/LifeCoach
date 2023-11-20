c22ec0aba1f68c24a03014065b7b76512923789d
Juan Camilo Sánchez Urrego <jcsu2019369@gmail.com>
2023-11-20 15:33:19 -0500

    Feat: Logs

--- app/src/main/java/com/example/lifecoach_/controllers/activities_controllers/activity_dashboard/DashBoardUserController.kt
+++ app/src/main/java/com/example/lifecoach_/controllers/activities_controllers/activity_dashboard/DashBoardUserController.kt
@@ -15,11 +15,14 @@ class DashBoardUserController(
     private val usersService = UsersService()
     fun updatePictureListener(uid: String, callback: (User?) -> Unit) {
         val file = File(filesDir, UUID.randomUUID().toString() + ".jpg")
+        Log.i("USERIMAGE", "File creation result ${file.createNewFile()}")
         val destUri = FileProvider
             .getUriForFile(
                 context,
                 context.packageName + ".fileprovider",
                 file)
+
+        Log.i("USERIMAGE", "URI: ${destUri.toString()}")
 
         usersService.userListener(uid, destUri) {newUser ->
             Log.i("USERIMAGE", "Got User Image Update")
