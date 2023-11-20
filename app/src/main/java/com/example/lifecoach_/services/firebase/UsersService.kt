7387efa9696260f5b7de4bf46acf0c0c3fbbe9b6
Juan Camilo Sánchez Urrego <jcsu2019369@gmail.com>
2023-11-20 14:23:21 -0500

    Feat: Permite que el UpdateUser pueda actualizar la foto

--- app/src/main/java/com/example/lifecoach_/services/firebase/UsersService.kt
+++ app/src/main/java/com/example/lifecoach_/services/firebase/UsersService.kt
@@ -18,7 +18,7 @@ class UsersService {
                 saveNewUser(user, picUri, callback)
             } else {
                 Log.i("REGISTER", "User ${user.email} found at $docId")
-                updateUser(docId, user, callback)
+                updateUser(docId, user, null, callback)
             }
         }
     }
@@ -37,12 +37,19 @@ class UsersService {
         }
     }
 
-    private fun updateUser(
+    fun updateUser(
         docId: String,
         user: User,
+        picUri: Uri?,
         callback: () -> Unit
     ) {
-        userRepository.updateUser(docId, user, null, callback)
+        if (picUri != null) {
+            picRepository.saveImage(picUri) {picRef ->
+                userRepository.updateUser(docId, user, picRef, callback)
+            }
+        } else {
+            userRepository.updateUser(docId, user, null, callback)
+        }
     }
 
     private fun tryFindUser(
