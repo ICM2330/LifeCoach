e8fd2c7e0dc56d847abdc5d1f32d47b92fbd6f41
Juan Camilo Sánchez Urrego <jcsu2019369@gmail.com>
2023-11-20 15:14:19 -0500

    Feat: Implementa la Actualización de la Imagen en DashBoard

---
+++ app/src/main/java/com/example/lifecoach_/controllers/activities_controllers/activity_dashboard/DashBoardUserController.kt
@@ -0,0 +1,27 @@
+package com.example.lifecoach_.controllers.activities_controllers.activity_dashboard
+
+import android.content.Context
+import androidx.core.content.FileProvider
+import com.example.lifecoach_.model.User
+import com.example.lifecoach_.services.firebase.UsersService
+import java.io.File
+import java.util.UUID
+
+class DashBoardUserController(
+    val context: Context,
+    val filesDir: File
+) {
+    private val usersService = UsersService()
+    fun updatePictureListener(uid: String, callback: (User?) -> Unit) {
+        val file = File(filesDir, UUID.randomUUID().toString() + ".jpg")
+        val destUri = FileProvider
+            .getUriForFile(
+                context,
+                context.packageName + ".fileprovider",
+                file)
+
+        usersService.userListener(uid, destUri) {
+
+        }
+    }
+}
