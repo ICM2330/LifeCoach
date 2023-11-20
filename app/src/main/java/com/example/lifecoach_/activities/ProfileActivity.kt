70908588886b8b68f19e117f72baddeec1bc7965
Juan Camilo Sánchez Urrego <jcsu2019369@gmail.com>
2023-11-20 14:29:08 -0500

    Feat: Conecta el Upload Controller con la Actualización de Usuario en el Servicio

--- app/src/main/java/com/example/lifecoach_/activities/ProfileActivity.kt
+++ app/src/main/java/com/example/lifecoach_/activities/ProfileActivity.kt
@@ -15,6 +15,7 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.FileProvider
 import com.example.lifecoach_.R
 import com.example.lifecoach_.activities.friends.ChatMenuActivity
+import com.example.lifecoach_.controllers.activities_controllers.activity_profile.ProfileActivityUploadController
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
 import com.example.lifecoach_.databinding.ActivityProfileBinding
@@ -94,7 +95,8 @@ class ProfileActivity : AppCompatActivity() {
         }
     }
 
-    private val usersService = UsersService()
+    private val profileActivityUploadController =
+        ProfileActivityUploadController(baseContext)
 
     private fun uploadInfo(binding: ActivityProfileBinding, user: User) {
         binding.uploadInfoProfileButton.setOnClickListener {
@@ -110,10 +112,7 @@ class ProfileActivity : AppCompatActivity() {
                 user.email = binding.emailProfile.text.toString()
                 user.phone = binding.phoneProfile.text.toString().toLong()
 
-                usersService.registerUser(user, Uri.parse(user.picture)) {
-                    Toast.makeText(baseContext, "Datos cambiados con éxito",
-                        Toast.LENGTH_SHORT).show()
-                }
+                profileActivityUploadController.updateUser(user)
             }
         }
     }
---
+++ app/src/main/java/com/example/lifecoach_/controllers/activities_controllers/activity_profile/ProfileActivityUploadController.kt
@@ -0,0 +1,22 @@
+package com.example.lifecoach_.controllers.activities_controllers.activity_profile
+
+import android.content.Context
+import android.net.Uri
+import android.widget.Toast
+import com.example.lifecoach_.model.User
+import com.example.lifecoach_.services.firebase.UsersService
+
+class ProfileActivityUploadController(val context: Context) {
+    private val usersService = UsersService()
+
+    fun updateUser(user: User) {
+        val uri = Uri.parse(user.picture)
+        usersService.updateUser(user, uri) {
+            Toast.makeText(
+                context,
+                "Usuario Actualizado",
+                Toast.LENGTH_SHORT
+            ).show()
+        }
+    }
+}
